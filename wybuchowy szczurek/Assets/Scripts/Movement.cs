2702b3d3836129f8d2b59773aabc97bac777bcd5
Jakub Solarek <80827847+novy213@users.noreply.github.com>
2024-04-10 20:15:38 +0200

    add some assets

---
+++ wybuchowy szczurek/Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //napisz mi skypt do poruszanie sie postaci 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
